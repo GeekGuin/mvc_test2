561e2868c28bf485346b77c21ea327f3d61cc9b7
Samuel Alvarez <seam.2194@gmail.com>
2015-11-25 11:16:49 -0430

    Sistema de carpetas

---
+++ mvc/views/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang es>
+<head>
+	<title></title>
+	<meta name="title" content="{Título de la Página}">
+	<meta name="keywords" content="{keywords}">
+	<meta name="description" content="{descripción}">
+	<link type="text/css" rel="stylesheet" href="css/template.css">
+</head>
+<body>
+	
+</body>
+</html>
